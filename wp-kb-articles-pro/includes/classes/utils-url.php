ff876b38a1ac38e414f57d1c5e149628d42dd404
jaswsinc <jas@wsharks.com>
2015-04-09 04:29:35 -0800

    UI Enhancements
    - Closes https://github.com/websharks/wp-kb-articles/issues/99
    - Closes https://github.com/websharks/wp-kb-articles/issues/98
    - Closes https://github.com/websharks/wp-kb-articles/issues/97

--- wp-kb-articles-pro/includes/classes/utils-url.php
+++ wp-kb-articles-pro/includes/classes/utils-url.php
@@ -786,10 +786,10 @@ namespace wp_kb_articles // Root namespace.
 				{
 					if(isset($qvs[$_qv])) continue; // Set already.
 
-					if(!empty($_REQUEST[$this->plugin->qv_prefix.$_qv]))
+					if(isset($_REQUEST[$this->plugin->qv_prefix.$_qv]))
 						$qvs[$_qv] = trim(stripslashes((string)$_REQUEST[$this->plugin->qv_prefix.$_qv]));
 
-					else if(($_qv_value = get_query_var($this->plugin->qv_prefix.$_qv)))
+					else if(!is_null($_qv_value = get_query_var($this->plugin->qv_prefix.$_qv, NULL)))
 						$qvs[$_qv] = (string)$_qv_value; // Current value.
 				}
 				unset($_qv, $_qv_value); // Housekeeping.
@@ -799,7 +799,7 @@ namespace wp_kb_articles // Root namespace.
 					$args = array(); // Initialize.
 					foreach($qvs as $_key => $_value)
 						if(($_key !== 'page' || $_value > 1) && strlen((string)$_value))
-							$args[$this->plugin->qv_prefix.$_key] = $_value;
+							$args[$this->plugin->qv_prefix.$_key] = (string)$_value;
 					unset($_key, $_value); // Housekeeping.
 
 					$url = add_query_arg(urlencode_deep($args), $url);
@@ -809,7 +809,7 @@ namespace wp_kb_articles // Root namespace.
 					$url = rtrim($url, '/');
 					foreach($qvs as $_key => $_value)
 						if(($_key !== 'page' || $_value > 1) && strlen((string)$_value))
-							$url .= '/'.urlencode($this->plugin->rewrite_prefix.$_key).'/'.urlencode($_value);
+							$url .= '/'.urlencode($this->plugin->rewrite_prefix.$_key).'/'.urlencode((string)$_value);
 					unset($_key, $_value); // Housekeeping.
 
 					$url = user_trailingslashit($url);
