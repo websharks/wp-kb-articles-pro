d4ca2cd4d4ba8f8149f3568057bf8a152a7b73e6
jaswsinc <jas@wsharks.com>
2015-04-10 05:28:24 -0800

    Adding new query class + index table.

--- wp-kb-articles-pro/includes/classes/sc-list.php
+++ wp-kb-articles-pro/includes/classes/sc-list.php
@@ -156,10 +156,10 @@ namespace wp_kb_articles // Root namespace.
 				if($this->attr->per_page > $upper_max_limit)
 					$this->attr->per_page = $upper_max_limit;
 
-				$_orderbys           = preg_split('/,+/', $this->attr->orderby, NULL, PREG_SPLIT_NO_EMPTY);
-				$_orderbys           = $this->plugin->utils_array->remove_emptys($this->plugin->utils_string->trim_deep($_orderbys));
+				$_orderbys            = preg_split('/,+/', $this->attr->orderby, NULL, PREG_SPLIT_NO_EMPTY);
+				$_orderbys            = $this->plugin->utils_array->remove_emptys($this->plugin->utils_string->trim_deep($_orderbys));
 				$this->attr->orderbys = $_orderbys; // Store these for use in template files.
-				$this->attr->orderby = array(); // Reset; convert to an associative array.
+				$this->attr->orderby  = array(); // Reset; convert to an associative array.
 				foreach($_orderbys as $_orderby) // Validate each orderby.
 				{
 					if(strpos($_orderby, ':', 1) === FALSE)
